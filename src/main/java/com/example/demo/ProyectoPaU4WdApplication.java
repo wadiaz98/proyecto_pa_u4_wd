9575904ae3f851174320852fbb3091f82ee17062
Willan Diaz <wadiaz@uce.edu.ec>
2023-02-24 18:52:36 -0500

    Subida de proyecto

---
+++ src/main/java/com/example/demo/ProyectoPaU4WdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU4WdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU4WdApplication.class, args);
+	}
+
+}
